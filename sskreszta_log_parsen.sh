2d38e111728a722e83780dedaed1d11cb8994e54
Arne Babenhauserheide <bab@draketo.de>
2009-09-28 10:51:40 +0200

    minor

--- sskreszta_log_parsen.sh
+++ sskreszta_log_parsen.sh
@@ -11,5 +11,6 @@ parse_and_list_markdown_files.py "Sskreszta-Log statisch" . http://1w6.org
 # Also turn the dot file into the travel diagram (png)
 echo "./aufruf_dot_zu_png.sh"
 ./aufruf_dot_zu_png.sh
+
 # And copy the current dot file into static
 cp stationen_unserer_reise.dot static/
