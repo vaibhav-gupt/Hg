c8bf608b808f11ac35e07f35c8c0ec7b810a16fe
Arne Babenhauserheide <bab@draketo.de>
2009-11-15 00:00:01 +0100

    Die Esperanto-?bersetzung landet nun automatisch auf http://1w6.org/1k6-regularo/

--- hauptdokument_parsen.sh
+++ hauptdokument_parsen.sh
@@ -2,6 +2,7 @@
 
 # a rather complex parse_and_list_markdown_files.py call :)
 
+# German version
 parse_and_list_markdown_files.py --preprocessor "lines = markdown_data.splitlines()
 for l in lines:
  _l = l
@@ -17,3 +18,22 @@ for l in lines:
  lines[lines.index(l)] = _l
 markdown_data = '\n'.join(lines)
 " "Ein Wuerfel System" Hauptdokument/Scribus-Quellen http://1w6.org/deutsch/regeln/quellen
+
+
+# Esperanto Version 
+
+parse_and_list_markdown_files.py --preprocessor "lines = markdown_data.splitlines()
+for l in lines:
+ _l = l
+ while _l and _l[-1] == ' ':
+  _l = _l[:-1] # remove trailing whitespace
+ _l = _l + '  ' # add exactly two spaces, since we don't want to concatenate lines
+ if _l.startswith('**'):
+  _l = _l[:-2] + '**  '
+ elif _l.startswith('*'):
+  _l = _l[:-2] + '*  '
+ elif _l.startswith('<fuss>'):
+  _l = '<small>' + _l[6:-2] + '</small>  '
+ lines[lines.index(l)] = _l
+markdown_data = '\n'.join(lines)
+" "Unu Kubo Kodo" Esperanto/Hauptdokument/Scribus-fontoj http://1w6.org/esperanto/
