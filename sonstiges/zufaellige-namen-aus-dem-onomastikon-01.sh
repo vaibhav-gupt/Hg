99209824653c84a31883965a012aad9afa62168e
Arne Babenhauserheide <arne@draketo.de>
2012-06-07 11:45:39 +0200

    impro

---
+++ sonstiges/zufaellige-namen-aus-dem-onomastikon-01.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+
+wget -m http://www.gaminggeeks.org/Resources/KateMonk/
+cd www.gaminggeeks.org/Resources/KateMonk/
+grep \<td\> -r */ |sed "s/<td>//" | sed "s/<\/td>//" > alle-namen.txt
+    
+python3 -c '
+def is_good_line(l): 
+        return l.split(":")[1:] and (
+        not "Male" in l and  
+        not "Female" in l and  
+        not ".html" in l and  
+        not "nbsp" in l and  
+        not "href" in l and  
+        not "Surname" in l and  
+        not "index." in l and  
+        not "ources." in l and  
+        not "otes." in l and 
+        not "ntro." in l and 
+        not "/" in l.split(":")[1] and  
+        not "+" in l.split(":")[1] and  
+        not "-" in l.split(":")[1] and  
+        not ";" in l and # letters which couldn’t be unescaped.
+        not "<FONT" in l and 
+        not " yr" in l and
+        not "son of " in l.split(":")[1] and  
+        l.split(":")[1].lstrip() and  
+        not l.split(":")[2:]
+        )
+
+ 
+import re, html.entities
+
+##
+# Removes HTML or XML character references and entities from a text string.
+#
+# @param text The HTML (or XML) source text.
+# @return The plain text.
+# thanks to http://effbot.org/zone/re-sub.htm#unescape-html
+
+def unescape(text):
+    def fixup(m):
+        text = m.group(0)
+        if text[:2] == "&#":
+            # character reference
+            try:
+                if text[:3] == "&#x":
+                    return chr(int(text[3:-1], 16))
+                else:
+                    return chr(int(text[2:-1]))
+            except ValueError:
+                pass
+        else:
+            # named entity
+            try:
+                text = chr(html.entities.name2codepoint[text[1:-1]])
+            except KeyError:
+                pass
+        return text # leave as is
+    return re.sub("&#?\w+;", fixup, text)    
+    
+with open("alle-namen.txt", encoding="ISO-8859-15") as namen: 
+        names = [(j,i) for i,j in (line.split(":")
+                 for line in (unescape(l) for l in namen) 
+                 if is_good_line(line))]
+    
+with open("alle-namen-vorne.txt", "w", encoding="utf-8") as f:  
+        for i,j in names:  
+            k = i[:-2] + "            " + j + "\n"
+            f.write(k.lstrip())
+'
+
+cp alle-namen-vorne.txt ../../../
+
+# 12 zufällige Namen rausholen: 
+echo "cat alle-namen-vorne.txt | shuf | head -n 12"
