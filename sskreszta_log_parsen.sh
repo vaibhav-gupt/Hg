3d1ae22ea4f9f5dbd066cdc8a9883cb3ee8e776f
Martin Buerbaum <martin.buerbaum@gmx.at>
2012-10-13 09:36:40 +0200

    Changed all image paths to be relative.

---
+++ sskreszta_log_parsen.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Build the Sskreszta Log site. 
+# License of this script: GPL
+
+echo "entering folder RaumZeit/Charaktere/Sskreszta/"
+cd RaumZeit/Charaktere/Sskreszta/
+
+echo "parsing all files to html via pymarkdown_minisite" 
+parse_and_list_markdown_files.py "Sskreszta-Log statisch" . http://1w6.org
+
+# Also turn the dot file into the travel diagram (png)
+echo "./aufruf_dot_zu_png.sh"
+./aufruf_dot_zu_png.sh
+
+# And copy the current dot file into static
+cp stationen_unserer_reise.dot static/
