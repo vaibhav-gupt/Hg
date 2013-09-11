fc3a4a0102c57a2b7d61c567fa539eb47b9707b3
Arne Babenhauserheide <bab@draketo.de>
2013-09-11 02:10:33 +0200

    ews-dok Quellen: sauberere Referenzierung der K?nstler.

---
+++ sonstiges/Skripte/release_2.6.0.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# turn the 2.3.8 pdf into the 2.4 pdf with license and charsheet.
+
+## Screen Versions
+#pdfjoin ../../releases/1w6-regeln-2.4.0.pdf ../GPLv3.pdf ../../releases/charheft-vobsy.pdf --outfile ews-dok-2.4.0.pdf
+pdfjoin --no-landscape  --rotateoversize 'false' ../../releases/1w6-regeln-2.6.0.pdf ../../releases/charheft-vobsy.pdf --outfile 1w6-regeln-2.6.0.pdf
+pdfjoin --no-landscape  --rotateoversize 'false' ../../releases/1w6-regeln-2.6.0.pdf ../GPLv3-4-seiten.pdf ../../releases/charheft-vobsy.pdf --outfile 1w6-regeln-2.6.0-mit-lizenz.pdf
+
+## Booklet
+pdfnup --no-landscape --nup 1x1 --outfile part1.pdf --  ../../releases/1w6-regeln-2.6.0.pdf  1-24
+ pdfnup --no-landscape --nup 1x1 --outfile part2.pdf -- ../../releases/1w6-regeln-2.6.0.pdf 25-48
+ pdfnup --no-landscape --nup 1x1 --outfile gpl-1.pdf -- ../GPLv3-4-seiten.pdf 1-2
+ pdfnup --no-landscape --nup 1x1 --outfile gpl-2.pdf --  ../GPLv3-4-seiten.pdf 3-4
+ pdfnup --no-landscape  --nup 1x1 --outfile charheft.pdf -- ../../releases/charheft-vobsy.pdf  2,3,4,1
+ pdfjoin part1.pdf gpl-1.pdf charheft.pdf gpl-2.pdf part2.pdf --outfile test.pdf
+./write_booklet_pdfjam.py test.pdf 1w6-regeln-2.6.0-booklet.pdf
