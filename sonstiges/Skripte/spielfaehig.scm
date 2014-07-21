40db4dbed9f2806f4b61a988c51529d9c4738168
drak <drak@kaverne>
2014-07-21 17:30:45 +0200

    added spielf?hig script in scheme

---
+++ sonstiges/Skripte/spielfaehig.scm
@@ -0,0 +1,25 @@
+#!/usr/bin/env guile-2.0
+!#
+
+(define-module (spielfaehig)
+  #:export (spielfähig))
+(use-modules (srfi srfi-1)) ; for iota with count and start
+
+(define (factorial n)
+  (if (zero? n) 1 
+      (* n (factorial (- n 1)))))
+
+(define (nük n k)
+  (if (> k n) 0
+      (/ (factorial n) 
+         (factorial k) 
+         (factorial (- n k)))))
+
+(define (binom p n k)
+  (* (nük n k) (expt p k) (expt (- 1 p) (- n k))))
+
+(define (spielfähig p n min_spieler) 
+  (apply + 
+         (map (lambda (k) (binom p n k)) 
+              (iota (1+ (- n min_spieler)) min_spieler))))           
+
