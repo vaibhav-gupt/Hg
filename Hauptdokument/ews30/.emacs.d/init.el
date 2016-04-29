61047d10141bd51866444a7889346c1554bf049c
Arne Babenhauserheide <bab@draketo.de>
2016-04-29 19:00:45 +0200

    prepare shared emacs environment

---
+++ Hauptdokument/ews30/.emacs.d/init.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t)
+ '(user-full-name "1w6 Team"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(require 'ox-html)
+(require 'ox-latex)
