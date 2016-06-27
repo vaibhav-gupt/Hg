220c927f8b7ca336a0080c21b85d8b61e23601ce
Arne Babenhauserheide <bab@draketo.de>
2016-06-27 21:13:04 +0200

    absolutely awesome performance tricks for spielf?hig.py by bertm

--- sonstiges/Skripte/spielfaehig.py
+++ sonstiges/Skripte/spielfaehig.py
@@ -12,6 +12,7 @@ __copyright__ = """© 2010 Arne Babenhauserheide
 
 You can use this under the GNU GPLv3 or later, if you provide the correct license texts and such → http://gnu.org/licenses/gpl.html"""
 
+# simple binomials
 from math import factorial
 fac = factorial # ja, ich bin faul :)
 def nük(n, k): 
@@ -21,10 +22,28 @@ def nük(n, k):
 def binom(p, n, k): 
    return nük(n, k) * p** k * (1-p)**(n-k)
 
+# fast binomials, thanks to bertm
+from math import log, exp
+from itertools import accumulate
+def precalc_binom(p, n):
+    # log(0!), log(1!), log(2!), ..., log(n!)
+    log_facs = [1] + list(accumulate([log(i) for i in range(1, n + 1)]))
+    log_p = log(p)
+    log_1mp = log(1 - p)
+    def log_combs(k):
+        # Ensure symmetry
+        k = min(k, n - k)
+        return log_facs[n] - log_facs[n - k] - log_facs[k]
+    def binom(k):
+        return exp(log_combs(k) + log_p * k + log_1mp * (n - k))
+    return binom
+
+
 def spielfähig(p, n, min_spieler): 
-   try: 
-      return sum([binom(p, n, k) for k in range(min_spieler, n+1)])
-   except ValueError: return 1.0
+    binom = precalc_binom(p, n)
+    try:
+        return sum([binom(k) for k in range(min_spieler, n+1)])
+    except ValueError: return 1.0
 
 if __name__ == "__main__":
     from sys import argv
