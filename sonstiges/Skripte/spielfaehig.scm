83965f5231e308ca60efe154122ff9d4e2802f1a
drak <drak@kaverne>
2016-06-27 20:02:07 +0200

    Optimize spielf?hig.scm (thanks to bertm for the math).

--- sonstiges/Skripte/spielfaehig.scm
+++ sonstiges/Skripte/spielfaehig.scm
@@ -7,6 +7,8 @@ exec guile -e '(@@ (spielfaehig) main)' -s "$0" "$@"
   #:export (spielfähig main))
 (use-modules (srfi srfi-1) ; for iota with count and start
              (ice-9 match))
+
+;; base n!/(n! * (n-k)!) implementation
 (define (factorial n)
   (if (zero? n) 1 
       (* n (factorial (1- n)))))
@@ -17,15 +19,37 @@ exec guile -e '(@@ (spielfaehig) main)' -s "$0" "$@"
          (factorial k) 
          (factorial (- n k)))))
 
+;; optimize nük für small k and huge n
+(define (n!/n-k!_reference n k)
+  (if (<= k 1) n
+      (* (- n (- k 1))
+         (n!/n-k! n (- k 1)))))
+
+;; more efficient implementation (tight inner loop)
+(define (n!/n-k! n k)
+  "(n-k+1) * (n-k+2) * (n-k+...) * n)"
+  (if (zero? k) (error "k is 0, but must be > 0")
+      (let lp ((k-i (- k 1)))
+        (if (zero? k-i) n
+            (* (- n k-i)
+               (lp (- k-i 1)))))))
+
+(define (nük/small-k n k)
+  (if (> k n) 0
+      (/ (n!/n-k! n k)
+         (factorial k))))
+
+;; binom using the optimized nük
 (define (binom p n k)
-  (* (nük n k) 
+  (* (nük/small-k n k)
      (expt p k) 
      (expt (- 1 p) (- n k))))
 
+;; the actual interface: probability of having enough people to play at any given gaming night
 (define (spielfähig p n min_spieler) 
   (apply + 
          (map (lambda (k) (binom p n k)) 
-              (iota (1+ (- n min_spieler)) min_spieler))))           
+              (iota (1+ (- n min_spieler)) min_spieler))))
 
 
 (define (main args)
